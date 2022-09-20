c3f8e9d1c682d1b4b73df57b31ed3e0fb841207c
Abs2222 <88541981+Abs2222@users.noreply.github.com>
2022-09-20 11:27:02 -0300

    Add files via upload

---
+++ proyecto_estatico/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from aplicacion import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('aplicacion.urls')),
+]
+
+
+
